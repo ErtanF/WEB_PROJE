1d496579e231ed12b19c1da7fdfe0698cc3b877e
Ertan EFE <efeertan07@gmail.com>
2022-05-06 21:24:03 +0300

    iletisim bitiş

--- HTML/iletisimForm.php
+++ HTML/iletisimForm.php
@@ -3,9 +3,25 @@
 if(isset($_POST['Gonder']))
 {
     $v1 = $_POST['İsim'];
-    echo $v1;
-}
+    echo "İsim : " , $v1."<br>" ;
+    
+    $v2 = $_POST['Soyisim'];
+    echo "Soyisim : " , $v2."<br>" ;
 
-echo "Selam";
+    $v3 = $_POST['Email'];
+    echo "Email : " , $v3."<br>" ;
+
+    $v4 = $_POST['Konu'];
+    echo "Konu : " , $v4."<br>" ;
+
+    $v5 = $_POST['Mesaj'];
+    echo "Mesaj : " , $v5."<br>" ;
+
+}
+if($v1 and $v2 and $v3 and $v4 and $v5){
+ 
+} else {
+header("location:iletisim.html");
+}
 
 ?>
